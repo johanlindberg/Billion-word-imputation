2aa3ef1e58fdb024b4723f3c041d0cda9b19429a
Johan Lindberg <johan@pulp.se>
2014-09-23 10:49:08 +0200

    Added code to read the training-file line by line.

--- sep14.py
+++ sep14.py
@@ -1,10 +1,26 @@
 ## September 2014 attempt at Kaggles Billion Word Imputation
 ##
 
+## For each sentence in the test set, we have removed exactly one word.
+## Participants must create a model capable of inserting back the correct
+## missing word at the correct location in the sentence. Submissions are
+## scored using an edit distance to allow for partial credit.
+
 import sys
 
+N = 10
+
 def train(training_file):
-    pass
+    n = 0
+    f = open(training_file)
+    for line in f:
+        n += 1
+        if n > N:
+            break
+
+        print line
+
+    f.close()
 
 if __name__ == "__main__":
     if len(sys.argv) == 2:
