6e77d5a1bdd39b384c2f275d96770717879e8c32
Johan Lindberg <johan@pulp.se>
2014-09-24 10:48:46 +0200

    Lots of fixes and clarifications. Added a heart beat with some stats.
    Comments to clarify the not-so-simple command line arguments parsing and
    also some code to deal with default values.
    
    The heart beat prints how many lines have been read so far and how many
    words have been recognized. It also presents how many of those words are
    unique. It prints every 1 000 000 lines.

--- sep14.py
+++ sep14.py
@@ -20,14 +20,26 @@ import sys
 bigrams = {}
 
 def train(training_file, max_limit):
-    n = 0
     f = open(training_file)
+    line_count = 0
+    word_count = 0
     for line in f:
-        n += 1
-        if n > max_limit:
+        line_count += 1
+
+        max_limit -= 1
+        if max_limit == 0:
             break
 
+        # heart beat
+        if line_count % 1000000 == 0:
+            print "%d rows, %d/%d words %02.2f%% unique" \
+                  % (line_count,
+                     len(bigrams),
+                     word_count,
+                     (float(len(bigrams))/word_count) * 100)
+
         words = line.split()
+        word_count += len(words)
         for i in xrange(len(words) - 1):
             a, b = words[i], words[i+1]
 
@@ -49,28 +61,47 @@ def train(training_file, max_limit):
 
 if __name__ == "__main__":
     import copy
-    argv = copy.copy(sys.argv)
+    argv = copy.copy(sys.argv[1:]) # first arg is filename so
+                                   # it can safely be ignored
 
-    kwargs = { "max_limit": int,
-               "training_file": str, }
+    ## Match command line arguments against the kwargs spec
+    ## ----------------------------------------------------
+    kwargs = { "max_limit": (int, -1),
+               "training_file": (str, None), }
     for key in kwargs.keys():
         for prefix, end_of_slice in [("--", None), ("-", 1)]:
+            # keyword becomes, for example --max-limit and -m
             keyword = "%s%s" % (prefix, key[0:end_of_slice].replace("_", "-"))
             if keyword in argv:
                 try:
                     position = argv.index(keyword)
-                    kwargs[key] = kwargs[key](argv[position+1])
+                    # to make sure the value is of the correct type we
+                    # run it through a type converter function
+                    kwargs[key] = kwargs[key][0](argv[position+1])
 
-                    # remove from argv
+                    # remove this keyword and value from argv
                     argv = argv[0:position] + argv[position+2:]
 
+                except ValueError:
+                    raise Exception("The flag %s requires a %s"
+                                    % (keyword, str(kwargs[key])))
                 except IndexError:
-                    raise Exception("The flag %s requires a value." % (keyword))
+                    raise Exception("The flag %s requires a value."
+                                    % (keyword))
 
-    if len(argv) == 2:
-        kwargs["training_file"] = argv[1]
-    else:
+    # any argument left over is a value for training_file
+    if len(argv) == 1:
+        kwargs["training_file"] = argv[0]
+    elif len(argv) > 1:
         raise Exception("You can't have more than one default argument.")
+
+    # populate kwargs with default values unless provided as arguments
+    for key in kwargs.keys():
+        if type(kwargs[key]) == tuple: 
+            # if the tuple with type and default value hasn't been
+            # replaced already we haven't got a value from the user
+            # so we replace it now
+            kwargs[key] = kwargs[key][1]
 
     train(**kwargs)
     
