9dfae61828bd4afb0fdefc7907ac2f8fad40e485
Johan Lindberg <johan@pulp.se>
2014-10-17 12:51:40 +0200

    Minor bugfixes and some straightening up of the code.

--- att1.py
+++ att1.py
@@ -6,17 +6,46 @@
 ## Submissions are scored using an edit distance to allow for partial
 ## credit.
 
+## Your submission file should contain the sentence id and a predicted
+## sentence. To prevent parsing issues, you should use double quotes to
+## escape the sentence text and two double quotes ("") for double
+## quotes within a sentence. Note that test.csv is a valid submission
+## file itself.
+##
+## The file should contain a header and have the following format:
+##
+##   id,"sentence"
+##   1,"Former Dodgers ... story after another ."
+##   2,"8 parliamentary ... ally against Islamic ."
+##   3,"Sales of drink ... from a small base ."
+##   etc...
+
 ## Attempt 1
 
 import test
 
 import sys
 
+def decode_sentence(sentence):
+    """Strip newlines and quotes from first and last position."""
+    sentence = sentence.strip("\n")
+    if sentence[0] == '"':
+        sentence = sentence[1:]
+    if sentence[-1] == '"':
+        sentence = sentence[:-1]
+
+    return sentence
+
+def encode_sentence(sentence):
+    """Change all " to "" in sentence.
+    NOTE! This is a stub. It just returns sentence as-is."""
+    return sentence
+
 def replace_missing_word(sentence):
     s = sentence.split()
     i = find_missing_word(sentence)
 
-    return " ".join(s[:i] + ["HELLO"] + s[i+1:])
+    return " ".join(s[:i] + ["HELLO"] + s[i:])
 
 def find_missing_word(sentence):
     """NOTE! This method is a stub.
@@ -32,8 +61,9 @@ def replace_words(test_file, submission_file):
         f.readline() # get rid of headers
         for line in f:
             id, sentence = line.split(",")
-            s = replace_missing_word(sentence)
-            submission.append('%s,"%s"' % (id, s))
+
+            s = replace_missing_word(decode_sentence(sentence))
+            submission.append('%s,"%s"' % (id, encode_sentence(s)))
 
     ## write submission_file
     with open(submission_file, "w") as f:
