d1cdfb9eebbaf0a00042cc2c3edeaa64e35bb5ea
Johan Lindberg <johan@pulp.se>
2014-11-29 22:58:50 +0100

    Added code to actually produce the hashtable with bigrams.
    Unfortunately this is a lot slower than the Python version. Running it
    on SBCL causes a crash because it runs out of memory in the heap. CCL
    behaves better but is even slower. I added the *MAX-LINES* parameter
    so that I could make a comparison. 1 000 000 rows takes about five
    minutes with CCL and about 45 seconds in Python.

--- lisp/build.lisp
+++ lisp/build.lisp
@@ -8,12 +8,31 @@
 
 ;; Step 1. Build bigrams index
 
+(use-package :cl-ppcre)
+
+(defparameter *MAX-LINES* 100000) 
+(defparameter *HEART-BEAT-INTERVAL* 50000)
+
+(defvar *BIGRAMS* (make-hash-table :test 'equal))
+
 (defun process-file (filename)
   (with-open-file (f filename)
     (do ((line-index 0 (1+ line-index))
 	 (line (read-line f) (read-line f nil 'EOF)))
-	((eq line 'EOF) line-index)
+	((or (and *MAX-LINES*
+		  (>= line-index *MAX-LINES*))
+	     (eq line 'EOF))
+	 (values *BIGRAMS* line-index))
+      (when (eq 0 (mod line-index *HEART-BEAT-INTERVAL*))
+	(format t "."))
       (process-line line))))
 
 (defun process-line (line)
-  (format t "~&~A" line))
+  (let ((words (cl-ppcre:split "\\s+" line)))
+    (mapcar #'(lambda (w1 w2)
+		(let ((w1-bigrams (gethash w1 *BIGRAMS*)))
+		  (when (null w1-bigrams)
+		    (setf  w1-bigrams (make-hash-table :test 'equal)
+			  (gethash w1 *BIGRAMS*) w1-bigrams))
+		  (incf (gethash w2 w1-bigrams 0))))
+	    words (cdr words))))
