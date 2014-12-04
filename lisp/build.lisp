b69a5e62452314d570773dad25d3a041e6e77f4a
Johan Lindberg <johan@pulp.se>
2014-12-04 23:25:43 +0100

    The process-file now spits out a file with source code.
    The idea is to create a source file containing all the lisp code to
    produce the bigrams hashtable. It's perhaps not the prettiest of
    approaches but it works and it looks promising. I'm assuming that I
    can scale this to many hundred thousands of lines of code.
    
    In other news, after some investigations it seems that SBCL and CCL
    are about four times slower than Python when it comes to IO. So there.
    That explains the poor performance in build.lisp but all will be
    forgiven and forgotten as long as they turn out to be blazing fast
    in the other areas.

--- lisp/build.lisp
+++ lisp/build.lisp
@@ -10,31 +10,44 @@
 
 (use-package :cl-ppcre)
 
-(defparameter *MAX-LINES* 100000) 
-(defparameter *HEART-BEAT-INTERVAL* 50000)
+(defparameter *MAX-LINES* 10) 
+(defparameter *HEART-BEAT-INTERVAL* 100000)
 
-(defvar *BIGRAMS* (make-hash-table :test 'equal))
+(defparameter *BIGRAMS* (make-hash-table :test 'equal))
 
-(defun process-file (filename)
-  (with-open-file (f filename)
-    (do ((line-index 0 (1+ line-index))
-	 (line (read-line f) (read-line f nil 'EOF)))
-	((or (and *MAX-LINES*
-		  (>= line-index *MAX-LINES*))
-	     (eq line 'EOF))
-	 (values *BIGRAMS* line-index))
-      (when (eq 0 (mod line-index *HEART-BEAT-INTERVAL*))
-	(format t "."))
-      (process-line line))))
+(defparameter *tf* ; training file on SD Card
+  "/media/removable/SD Card/Billion-Word-Imputation/train_v2.txt")
+(defparameter *to*
+  "~/Downloads/Playground/Kaggle/Billion-Word-Imputation/lisp/bigrams.lisp")
+
+(defun process-file (filename store)
+  "process-file reads the contents of <filename> and produces lisp code to
+   produce the bigrams hash-table which is saved to <store>."
+  (with-open-file (out store :direction :output :if-exists :supersede)
+    (with-open-file (in filename)
+      (do ((line-index 0 (1+ line-index))
+	   (line (read-line in) (read-line in nil 'EOF)))
+	  ((or (and *MAX-LINES*
+		    (>= line-index *MAX-LINES*))
+	       (eq line 'EOF))
+	   (values *BIGRAMS* line-index))
+	(when (eq 0 (mod line-index *HEART-BEAT-INTERVAL*))
+	  (format t "."))
+	; -- process-line produces a string of lisp code
+	(write-line (process-line line) out)))))
 
 (defun process-line (line)
-  (let ((words (cl-ppcre:split "\\s+" line)))
+  (let ((words (cl-ppcre:split "\\s+" line))
+	(result ""))
     (do ((i 1 (1+ i)))
-	((> i (length words)))
+	((>= i (length words)))
       (let* ((w1 (nth (- i 1) words))
 	     (w2 (nth i words))
-	     (w1-bigrams (gethash w1 *BIGRAMS*)))
-	(when (null w1-bigrams)
-	  (setf  w1-bigrams (make-hash-table :test 'equal)
-		 (gethash w1 *BIGRAMS*) w1-bigrams))
-	(incf (gethash w2 w1-bigrams 0))))))
+	     (*print-escape* t))
+	(unless (gethash w1 *BIGRAMS*)
+	  (setf (gethash w1 *BIGRAMS*) t 
+		result
+		(format nil "~A~%(setf (gethash ~W *BIGRAMS*) (make-hash-table :test 'equal))" result w1)))
+	(setq result
+	      (format nil "~A~%(incf (gethash ~W (gethash ~W *BIGRAMS*) 0))" result w2 w1))))
+    result))
